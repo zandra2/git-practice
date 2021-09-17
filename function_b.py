eef2497e7fe4df5f3d258c5f23caee6146ae86e4
Zandra Nguyen <zandra.eng@gmail.com>
2021-09-17 13:46:00 -0700

    add function b

--- function_b.py
+++ function_b.py
@@ -2,12 +2,22 @@
 # Iterating Over Data
 # Problem-Set While Loops #11
 def silly_sum():
-    """ reads numbers from the user (use input_int) 
+    """ reads numbers from the user
         summing as we go until either
         the user enters 0, or
         the sum reaches or exceeds 1000
     """
-    pass
+    num = int(input('Please enter a number => '))
+    sum = 0
+
+    while num != 0:
+        sum += num
+        if sum >= 1000:
+            break
+
+        num = int(input('Please enter a number => '))
+
+    return sum
 
 
 if __name__ == "__main__":
